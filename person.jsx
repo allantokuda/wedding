4fc1e074340245ff26f7b517af85471b04611ead
Allan Tokuda <praetis@gmail.com>
2015-02-21 22:00:39 -0600

    More info, input state, better style

--- person.jsx
+++ person.jsx
@@ -1,4 +1,11 @@
 var Person = React.createClass({
+  getInitialState: function() {
+    return { accept: undefined, drinks: false, comments: "" }
+  },
+
+  yes: function() { this.setState({ accept: true  }); },
+  no:  function() { this.setState({ accept: false }); },
+
   render: function() {
     return (
       <div className="person panel panel-default">
@@ -9,19 +16,19 @@ var Person = React.createClass({
             </div>
             <div className="response col-sm-4">
               <label>
-                <input type="radio" name={ "accept" + this.props.personNumber } value="yes" />
-                Gladly accept
+                <input type="radio" name={ "accept" + this.props.personNumber } onClick={ this.yes } value="yes" />
+                <span>Gladly accept</span>
               </label><br />
               <label>
-                <input type="radio" name={ "accept" + this.props.personNumber } value="no" />
-                Unable to attend
+                <input type="radio" name={ "accept" + this.props.personNumber } onClick={ this.no } value="no" />
+                <span>Unable to attend</span>
               </label>
             </div>
             <div className="response col-sm-4">
               <br />
               <label>
                 <input type="checkbox" name={ "late" + this.props.personNumber } value="yes" />
-                Drinks after reception
+                <span>Will join for drinks after reception</span>
               </label><br />
             </div>
           </div>
