87f1f7e8721bc705f3c1b54b0f282ebc7bf7aadc
Allan Tokuda <praetis@gmail.com>
2015-03-01 19:01:42 -0600

    Add reaction upon RSVP button press

--- invitation.jsx
+++ invitation.jsx
@@ -17,6 +17,11 @@ var Invitation = React.createClass({
 
   send: function(e) {
     e.preventDefault();
+
+    // add timestamp to state and immediately send it to Firebase (requires direct manipulation and force update)
+    this.state.responseDate = Firebase.ServerValue.TIMESTAMP;
+    this.forceUpdate();
+
     this.firebaseRef.set(this.state);
   },
 
@@ -27,6 +32,7 @@ var Invitation = React.createClass({
   },
 
   render: function() {
+    console.log(this.state);
     if (this.state.people) {
       console.log(this.state.people[0]);
       console.log(this.state.people[1]);
@@ -36,12 +42,19 @@ var Invitation = React.createClass({
         );
       }, this);
 
+
+      var reactionTag;
+      if (this.state.responseDate) {
+        reactionTag = <div className="reaction panel"><div className="panel-body">Thank you for responding!</div></div>;
+      }
+
       return (
         <div className="invitation">
           {peopleSections}
           <div className="rsvp">
             <a className="btn btn-lg btn-default" href="#" onClick={this.send}>RSVP</a>
           </div>
+          { reactionTag }
         </div>
       );
     } else {
