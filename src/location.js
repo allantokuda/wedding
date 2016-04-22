75757399b26f1e08a737c879d2a668bc72f0c690
Allan Tokuda <praetis@gmail.com>
2016-04-22 14:39:38 -0500

    Add location component

---
+++ src/location.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+export default React.createClass({
+  render() {
+    let loc = this.props.location;
+    return (
+      <div>
+        {loc.what && <h2>{loc.what}</h2>}
+        {loc.when && <p>{loc.when}</p>}
+        <p>
+          {loc.where}
+          {loc.map     && <span> (<a href={loc.map    }>Map</a>)</span>}
+          {loc.website && <span> (<a href={loc.website}>Website</a>)</span>}
+        </p>
+        {loc.how && <p className="details">{loc.how}</p>}
+      </div>
+    );
+  }
+});
