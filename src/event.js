84a410529c4562d341a0f326edd1f8f949478aca
Allan Tokuda <praetis@gmail.com>
2016-04-25 07:48:13 -0500

    Update event guestbook view

--- src/event.js
+++ src/event.js
@@ -11,46 +11,69 @@ export default React.createClass({
     this.eventRef = new Firebase(__DATABASE_LOCATION__ + '/event/' + this.props.params.eventId);
     this.eventRef.authWithOAuthPopup("google", (error, authData) => {
       if (error) {
-        console.error("Authentication Failed!", error);
+        console.error("Authentication failed!", error);
       } else {
         console.log("Authentication success!", authData);
-        
       }
     });
 
-    this.eventRef.on("value", function(eventSnapshot) {
+    this.eventRef.on("value", eventSnapshot => {
       let event = eventSnapshot.val();
-      console.log('event:', event);
-      this.invitationRef = new Firebase(firebaseLocation + '/invitation');
-      // TODO: only get invitations for the current event
-      this.invitationRef.on("value", function(invitationSnapshot) {
-        event.items = invitationSnapshot.val();
-        this.setState(event);
-      }.bind(this));
-    }.bind(this));
+      let card = event.card;
+      let invitations = event.invitations;
+      this.setState({ card, invitations });
+    });
   },
 
   componentWillUnmount: function() {
-    this.firebaseRef.off();
+    this.eventRef.off();
   },
 
-  render: function() {
-    var result = _.map(this.state.items, (item, inviteId) => {
-      var inviteLink = "/invitation/" + inviteId
-      var emailLink = "mailto:" + item.email + "?subject=" + this.state.emailSubject + "&body=" + this.state.emailBody + "%0A%0A" + inviteLink
+  changeInvitation(invitationId, attributeName, e) {
+    let invitations = this.state.invitations;
+    invitations[invitationId][attributeName] = e.target.value;
+    this.setState({invitations});
+    this.eventRef.child('invitations/' + invitationId + '/' + attributeName).set(e.target.value);
+  },
 
-      var emailTag = <a href={emailLink}>{ item.email }</a>
-      var inviteTag = <a href={inviteLink}>Invitation</a>
+  render() {
+    let result = _.map(this.state.invitations, (item, inviteId) => {
+      var inviteLink = "/event/" + this.props.params.eventId + '/' + inviteId
 
-      var people = _.map(item.people, function(person, i) {
+      //var emailLink = "mailto:" + item.email + "?subject=" + this.state.card.emailSubject + "&body=" + this.state.card.emailBody + "%0A%0A" + inviteLink
+
+      let numPeople = item.people.length;
+
+      let emailCell = (
+        <td rowSpan={numPeople}>
+          <input name="email" type="text" value={item.email} onChange={this.changeInvitation.bind(this, inviteId, 'email')}/>
+        </td>
+      );
+
+      let invitationLinkCell = (
+        <td rowSpan={numPeople}>
+          <a target="_blank" href={inviteLink}>Invitation</a>
+        </td>
+      );
+
+      let commentsCell = (
+        <td rowSpan={numPeople}>{item.comments}</td>
+      );
+
+      var people = _.map(item.people, (person, i) => {
+        let questionCells = this.state.card.individualQuestions.map((question, i) => {
+          <td key={i}>{ person[question.name] || '-' }</td>
+        });
+
         return (
-          <tr style={ i == 0 ? { fontWeight: 'bold' } : {} }>
+          <tr>
+            {i == 0 && emailCell}
+            {i == 0 && invitationLinkCell}
             <td>{ person.name || '+1' }</td>
-            <td>{ i == 0 ? emailTag : null }</td>
-            <td>{ i == 0 ? inviteTag : null }</td>
             <td>{ person.accept || '-' }</td>
             <td>{ person.drinks || '-' }</td>
-            <td>{ i == 0 ? item.comments : '-' }</td>
+            {questionCells}
+            {i == 0 && commentsCell}
           </tr>
         );
       });
@@ -58,15 +81,20 @@ export default React.createClass({
       return people;
     });
 
+    let headerCells = this.state.card && this.state.card.individualQuestions.map((question, i) => {
+      return (
+        <th key={i}>{ question.label || question.name }</th>
+      );
+    });
+
     return (
       <table className="table">
         <thead>
           <tr>
-            <th>Name</th>
-            <th>Invitation email</th>
+            <th>Email</th>
             <th>Invitation</th>
-            <th>Accept?</th>
-            <th>Drinks?</th>
+            <th>Name</th>
+            {headerCells}
             <th>Comments</th>
           </tr>
         </thead>
